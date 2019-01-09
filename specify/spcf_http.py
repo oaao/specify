7ec661022caef2a15a003cfa883e219228107fcf
oaao <oliver.antoniu@gmail.com>
2019-01-08 22:29:51 -0700

    add [http]: helper structures + functions

---
+++ specify/spcf_http.py
@@ -0,0 +1,25 @@
+# helper structures and functions for Specify's HTTP interactions
+
+# configuration dependencies
+from specify.config import SPECIFY_SERVER, SPECIFY_AUTH
+
+# data formatting dependencies
+from typing import Dict, List
+from urllib import parse
+
+AUTH_QUERY: Dict[str, str] = {
+    "response_type": "code",
+    "redirect_uri":  SPECIFY_SERVER['uri_redirect'].format(SPECIFY_SERVER['url_base'], SPECIFY_SERVER['port']),
+    "scope":         SPECIFY_SERVER['scopes'],
+    "client_id":     SPECIFY_AUTH['client_id']
+    }
+
+
+def format_url_query(url: str, params: dict) -> str:
+
+    param_pairs:   List[str]    = ["{}={}".format(field, parse.quote(content)) for field, content in params.items()]
+    concat_params: str          = "&".join(param_pairs)
+
+    url_query:     str          = "{}/?{}".format(url, concat_params)
+
+    return url_query
