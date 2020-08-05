9eb2890d54b971013932610e88639cffa8d488f5
oaao <oliver.antoniu@gmail.com>
2020-08-04 20:27:49 -0600

    rev selector - changing templates & playing around

--- specify/utils.py
+++ specify/utils.py
@@ -10,13 +10,14 @@ def selector(action, headers):
     API:     str            = SPOTIFY_API['url_base'] + SPOTIFY_API['api_version']
 
     ACTIONS: Dict[str, str] = {
-        'generic':      {'template': 'table.html',        'path': '{}'}, # accommodate this
-        'user':         {'template': 'unknown',           'path': 'me/'},
-        'current_song': {'template': 'current_song.html', 'path': 'me/player/currently-playing'},
-        'analysis':     {'template': 'unknown',           'path': 'audio/analysis/{}'}, # accommodate this
-        'top_tracks':   {'template': 'table.html',        'path': 'me/'},
-        'top_artists':  {'template': 'top_artists.html',  'path': 'me/top/artists'},
-        'top_tracks':   {'template': 'top_tracks.html',   'path': 'me/top/tracks'},
+        'generic':         {'template': 'table.html',        'path': '{}'}, # accommodate this
+        'user':            {'template': 'table.html',        'path': 'me/'},
+        'current_song':    {'template': 'current_song.html', 'path': 'me/player/currently-playing'},
+        'analysis':        {'template': 'unknown',           'path': 'audio/analysis/{}'}, # accommodate this
+        'top_tracks':      {'template': 'table.html',        'path': 'me/'},
+        'top_artists':     {'template': 'top_artists.html',  'path': 'me/top/artists'},
+        'top_tracks':      {'template': 'top_tracks.html',   'path': 'me/top/tracks'},
+        'recommendations': {'template': 'table.html',        'path': '/recommendations'}
     }
 
     url:  str = f'{API}{ACTIONS[action]["path"]}'
@@ -26,6 +27,6 @@ def selector(action, headers):
     try:
         j    = json.loads(data)
     except ValueError:
-        raise ValueError('no currently playing song')
+        raise ValueError('ruh roh')
 
     return render_template(ACTIONS[action]["template"], data=j)
